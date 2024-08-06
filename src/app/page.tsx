1dedcafb3e87fa77fd8bd21b1eab7bdc0afef79c
Jayant Aggarwal <jadps2004@gmail.com>
2024-08-06 21:39:27 +0530

    fixed bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 import Content from "@/components/Content";
 import HeaderNavbar from "@/components/Header";
 import OnBoarding from "@/components/Onboarding";
-import PathBar from "@/components/TrackPage";
+import PathBar from "@/components/PathBar";
 import React from "react";
 
 const Home = () => {
   return (
     <div>
       <HeaderNavbar />
-
       <OnBoarding />
       <PathBar />
       <div className="flex justify-center">
